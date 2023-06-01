6458a431d07987ad284775ffc5bcea601da05d71
EstefaníaH <110538976+NiaHermida@users.noreply.github.com>
2023-06-01 19:59:04 -0300

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,7 @@
+const miBoton = document.getElementById('miBoton');
+
+function handleClick(){
+    alert('Hola! Haz hecho click en el botón.');
+}
+
+miBoton.addEventListener('click', handleClick);
